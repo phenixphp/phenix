210187d28c907bb6dc58fc11da229eb9c9ed570c
Barbosa <omar.barbosa@evertecinc.com>
2022-09-12 11:16:32 -0500

    Refactor: Update method accesibility

--- core/Facades/Response.php
+++ core/Facades/Response.php
@@ -5,12 +5,12 @@ namespace Core\Facades;
 use Core\Runtime\Facade;
 
 /**
- * @method static \Amp\Http\Server\Response plain(string $content, string $status = Status::OK)
- * @method static \Amp\Http\Server\Response json(string $content, string $status = Status::OK)
+ * @method static \Amp\Http\Server\Response plain(string $content, string $status = \Amp\Http\Status::OK)
+ * @method static \Amp\Http\Server\Response json(string $content, string $status = \Amp\Http\Status::OK)
  */
 class Response extends Facade
 {
-    public static function getKeyName(): string
+    protected static function getKeyName(): string
     {
         return 'response';
     }
