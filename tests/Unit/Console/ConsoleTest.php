f4238687937c350cec5d43ea05cba4147b99bc9a
Barbosa <omar.barbosa@evertecinc.com>
2022-09-22 05:48:29 -0500

    tests(wip): Trying test make controller command

--- tests/Unit/Console/ConsoleTest.php
+++ tests/Unit/Console/ConsoleTest.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Tests\Unit\Console;
 
 it('creates controller successfully', function () {
+    $this->app::swap(
+        \Core\Contracts\Filesystem\File::class,
+        \Tests\Mocks\File::class,
+    );
+
     $command = $this->phenix('make:controller', [
         'name' => 'TestController',
     ]);
