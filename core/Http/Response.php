3540e1a12a0837846f3869345db3fdfc0a3f13be
Barbosa <omar.barbosa@evertecinc.com>
2022-09-22 05:46:57 -0500

    fix: Change data type

--- core/Http/Response.php
+++ core/Http/Response.php
@@ -9,12 +9,12 @@ use Amp\Http\Status;
 
 class Response
 {
-    public function plain(string $content, string $status = Status::OK): ServerResponse
+    public function plain(string $content, int $status = Status::OK): ServerResponse
     {
         return new ServerResponse($status, ['content-type' => 'text/plain'], $content);
     }
 
-    public function json(array $content, string $status = Status::OK): ServerResponse
+    public function json(array $content, int $status = Status::OK): ServerResponse
     {
         return new ServerResponse($status, ['content-type' => 'application/javascript'], json_encode($content));
     }
