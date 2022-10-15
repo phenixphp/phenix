690b0497e01248390757e91f15c6acdbfade34cc
Barbosa <omar.barbosa@evertecinc.com>
2022-10-15 07:32:19 -0500

    test: makes controller with force option

--- tests/Unit/Console/ConsoleTest.php
+++ tests/Unit/Console/ConsoleTest.php
@@ -18,7 +18,6 @@ it('creates controller successfully', function () {
     /** @var \Symfony\Component\Console\Tester\CommandTester $command */
     $command = $this->phenix('make:controller', [
         'name' => 'TestController',
-        '--force' => true,
     ]);
 
     $command->assertCommandIsSuccessful();
@@ -31,15 +30,12 @@ it('creates controller successfully', function () {
 it('does not create the controller because it already exists', function () {
     $mock = mock(File::class)->expect(
         exists: fn (string $path) => true,
-        get: fn (string $path) => '',
-        put: fn (string $path) => true,
     );
 
     $this->app->swap(File::class, $mock);
 
     $this->phenix('make:controller', [
         'name' => 'TestController',
-        '--force' => true,
     ]);
 
     /** @var \Symfony\Component\Console\Tester\CommandTester $command */
@@ -52,6 +48,37 @@ it('does not create the controller because it already exists', function () {
     $output = $command->getDisplay();
 
     $this->assertStringContainsString('Controller already exists!', $output);
+});
+
+it('creates controller successfully with force option', function () {
+    $tempDir = sys_get_temp_dir();
+    $tempPath = $tempDir . DIRECTORY_SEPARATOR . 'TestController.php';
+
+    file_put_contents($tempPath, 'old content');
+
+    $this->assertEquals('old content', file_get_contents($tempPath));
+
+    $mock = mock(File::class)->expect(
+        exists: fn (string $path) => false,
+        get: fn (string $path) => 'new content',
+        put: fn (string $path, string $content) => file_put_contents($tempPath, $content),
+    );
+
+    $this->app->swap(File::class, $mock);
+
+    /** @var \Symfony\Component\Console\Tester\CommandTester $command */
+    $command = $this->phenix('make:controller', [
+        'name' => 'TestController',
+        '--force' => true,
+    ]);
+
+    $command->assertCommandIsSuccessful();
+
+    $output = $command->getDisplay();
+
+    $this->assertStringContainsString('Controller successfully generated!', $output);
+    $this->assertEquals('new content', file_get_contents($tempPath));
+
 });
 
 it('creates controller successfully in nested namespace', function () {
@@ -69,7 +96,6 @@ it('creates controller successfully in nested namespace', function () {
     /** @var \Symfony\Component\Console\Tester\CommandTester $command */
     $command = $this->phenix('make:controller', [
         'name' => 'Admin/UserController',
-        '--force' => true,
     ]);
 
     $command->assertCommandIsSuccessful();
