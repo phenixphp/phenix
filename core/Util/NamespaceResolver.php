1705df06b0d02d449d8f8fcf4ef4c3603ea45cf2
Barbosa <omar.barbosa@evertecinc.com>
2022-12-27 09:48:55 -0500

    refactor: get path from app

--- core/Util/NamespaceResolver.php
+++ core/Util/NamespaceResolver.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Core\Util;
 
+use Core\App;
+
 class NamespaceResolver
 {
     public static function parse(string $path): string
     {
-        $namespace = str_replace([APP_PATH . DIRECTORY_SEPARATOR, '.php', '/'], ['', '', '\\'], $path);
+        $base = App::path() . DIRECTORY_SEPARATOR;
+
+        $namespace = str_replace([$base, '.php', '/'], ['', '', '\\'], $path);
 
         return ucfirst($namespace);
     }
