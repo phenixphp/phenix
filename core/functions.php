8d09f9b2cdbe4c74567fdf4b417c54b2034bc161
Barbosa <omar.barbosa@evertecinc.com>
2022-12-27 10:36:19 -0500

    refactor: improve global functions

--- core/functions.php
+++ core/functions.php
@@ -6,30 +6,19 @@ use Core\App;
 use Core\Http\Response;
 
 if (! function_exists('base_path()')) {
-    function base_path(string $path = ''): string
-    {
-        if (str_starts_with($path, DIRECTORY_SEPARATOR)) {
-            $path = ltrim($path, DIRECTORY_SEPARATOR);
-        }
-
-        if (str_ends_with($path, DIRECTORY_SEPARATOR)) {
-            $path = rtrim($path, DIRECTORY_SEPARATOR);
-        }
-
-        return App::path() . DIRECTORY_SEPARATOR . $path;
+    function base_path(string $path = ''): string {
+        return App::path() . DIRECTORY_SEPARATOR . trim($path, DIRECTORY_SEPARATOR);
     }
 }
 
 if (! function_exists('response')) {
-    function response(): Response
-    {
+    function response(): Response {
         return new Response();
     }
 }
 
 if (! function_exists('env')) {
-    function env(string $key, callable $default): string|int|bool
-    {
+    function env(string $key, callable $default): string|int|bool {
         $value = getenv($key);
 
         return $value ? $value : $default();
