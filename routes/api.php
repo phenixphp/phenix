f0aba6f6fe6a1ab91037f277d98591cb7287a0f5
Omar Barbosa <omar.barbosa@evertecinc.com>
2026-03-13 12:15:56 -0500

    feat: implement ResendOtpController and corresponding tests for email verification OTP

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,26 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResendOtpController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\WelcomeController;
-use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\Guest;
+use Phenix\Cache\RateLimit\Middlewares\RateLimiter;
 use Phenix\Facades\Route;
 use Phenix\Routing\Route as Router;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
-Route::middleware(RedirectIfAuthenticated::class)->group(function (Router $router): void {
-    $router->post('register', [RegisterController::class, 'store'])
-        ->name('register');
+Route::middleware(Guest::class)
+    ->group(function (Router $router): void {
+        $router->post('register', [RegisterController::class, 'store'])
+            ->name('register');
 
-    $router->post('verify-email', [VerifyEmailController::class, 'verify'])
-        ->name('verification.verify');
-});
+        $router->post('verify-email', [VerifyEmailController::class, 'verify'])
+            ->name('verification.verify')
+            ->middleware(RateLimiter::perMinute(6));
+
+        $router->post('resend-verification-otp', [ResendOtpController::class, 'resend'])
+            ->name('verification.resend')
+            ->middleware(RateLimiter::perMinute(2));
+    });
